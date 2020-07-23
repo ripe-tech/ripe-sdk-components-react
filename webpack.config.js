7613358ab2a76e2f9bbbdb5c18d3e7930a087a44
Margarida Silva <t-ms@platforme.com>
2020-07-23 15:12:20 +0100

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+module.exports = {
+    entry: "./index.js",
+    output: {
+        library: "RipeSdkComponentsReact"
+    },
+    devServer: {
+        compress: false,
+        port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+        stats: "minimal",
+        hot: true,
+        historyApiFallback: true,
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        query: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                            plugins: [
+                                "@babel/plugin-proposal-class-properties",
+                                [
+                                    "@babel/plugin-transform-runtime",
+                                    {
+                                        regenerator: true
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        loader: "eslint-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ico)$/,
+                loader: "file-loader",
+                options: {
+                    name: (path, query) => {
+                        if (process.env.NODE_ENV === "development") {
+                            return "[path][name].[ext]?[hash]";
+                        }
+                        return "[contenthash].[ext]";
+                    },
+                    esModule: false
+                }
+            },
+            {
+                test: /\.(svga)$/,
+                loader: "file-loader",
+                options: {
+                    name: (path, query) => {
+                        if (process.env.NODE_ENV === "development") {
+                            return "[path][name].svg?[hash]";
+                        }
+                        return "[contenthash].svg";
+                    },
+                    esModule: false
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: "file-loader",
+                options: {
+                    esModule: false
+                }
+            }
+        ]
+    }
+};

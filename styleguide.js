d53400de8c8eb386cd4ac5f53e5b69ad5697af6a
Gabriel Candal <gcc@platforme.com>
2020-08-06 17:31:14 +0100

    Fix styleguide script

--- styleguide.js
+++ styleguide.js
@@ -1,4 +1,4 @@
-if (window.location.pathname === "/") {
+if (window.location.hostname.endsWith("vercel.app")) {
     const script = document.createElement("base");
     script.href = "/styleguide/";
     document.getElementsByTagName("head")[0].appendChild(script);

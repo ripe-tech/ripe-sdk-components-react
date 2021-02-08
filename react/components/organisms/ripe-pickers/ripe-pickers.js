4019948e4592e7cd8a81f10830868d192d482dfa
BeeMargarida <ms@platforme.com>
2021-02-08 17:39:43 +0000

    feat: pickers using global logic mixin

--- react/components/organisms/ripe-pickers/ripe-pickers.js
+++ react/components/organisms/ripe-pickers/ripe-pickers.js
@@ -1,52 +1,98 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Ripe } from "ripe-sdk";
+import { mix } from "yonius";
 
-export class RipePickers extends Component {
+import { LogicMixin } from "../../../mixins";
+
+export class RipePickers extends mix(Component).with(LogicMixin) {
     static get propTypes() {
         return {
-            /**
-             * An initialized RIPE instance form the RIPE SDK, if not defined,
-             * a new SDK instance will be initialized.
-             */
-            ripe: PropTypes.object,
+            ...this._propTypes,
             /**
              * Callback when a part of the model in the configurator is highlighted,
              * normally with a mouse hover of by changing the prop. Only functional
              * when masks are enabled.
              */
-            onUpdateHighlightedPart: PropTypes.func,
-            /**
-             * Callback when parts of the model are changed, meaning that a new
-             * customization was made.
-             */
-            onUpdateParts: PropTypes.func,
-            /**
-             * Callback when the configurator is loading.
-             */
-            onLoading: PropTypes.func,
-            /**
-             * Callback when the configurator has finished loading,
-             * when it is possible to visualize it or when an error occurred.
-             */
-            onLoaded: PropTypes.func
+            onUpdateHighlightedPart: PropTypes.func
         };
     }
 
     static get defaultProps() {
         return {
-            ripe: null,
-            onUpdateHighlightedPart: part => {},
-            onUpdateParts: parts => {},
-            onLoading: () => {},
-            onLoaded: () => {}
+            ...this._defaultProps,
+            onUpdateHighlightedPart: part => { }
         };
     }
 
     constructor(props) {
+        console.log("constructor pickers", props.ripe.brand);
         super(props);
 
         this.state = {
+            /**
+             * RIPE instance, which can be later initialized
+             * if the given prop is not defined.
+             */
+            ripeData: props.ripe,
+            /**
+             * Brand to be used for the internal sync operation.
+             */
+            brandData: props.brand,
+            /**
+             * Model to be used for the internal sync operation.
+             */
+            modelData: props.model,
+            /**
+             * 3DB version to be used for the internal sync operation.
+             */
+            versionData: props.version,
+            /**
+             * Currency to be used for the internal sync operation.
+             */
+            currencyData: props.currency,
+            /**
+             * Reflects whether this component should apply
+             * configuration changes to the associated RIPE SDK.
+             */
+            configData: props.config,
+            /**
+             * Parts of the model to be used for the internal sync
+             * operation.
+             */
+            partsData: props.parts,
+            /**
+             * Initials to be used for the internal sync operation.
+             */
+            initialsData: props.initials,
+            /**
+             * Engraving to be used for the internal sync operation.
+             */
+            engravingData: props.engraving,
+            /**
+             * Initials extra to be used for the internal sync operation.
+             */
+            initialsExtraData: props.initialsExtra,
+            /**
+             * Structure to be used for the internal sync operation.
+             */
+            structureData: props.structure,
+            /**
+             * Flag that controls if the initial loading process for
+             * the configurator is still running.
+             */
+            loading: true,
+            /**
+             * Flag that controls if the configuring process is
+             * still running.
+             */
+            configuring: false,
+            /**
+             * The object containing the complete set of part names
+             * associated with their current availability and available
+             * materials (which in turn should have the equivalent relation
+             * with the colors).
+             */
+            choices: {},
             /**
              * Part of the model currently selected in the dropdown and
              * highlighted in a possible existing configurator.
@@ -59,85 +105,44 @@ export class RipePickers extends Component {
             /**
              * Color selected for the currently selected part and of the model.
              */
-            selectedColor: null,
-            /**
-             * Model's customization options, which contains parts, materials
-             * and colors.
-             */
-            options: {},
-            /**
-             * Flag that controls if the initial loading process for
-             * the price is still running.
-             */
-            loading: true,
-            /**
-             * Ripe SDK instance, which can be later initialized
-             * if the given prop is not defined.
-             */
-            ripeData: this.props.ripe
+            selectedColor: null
         };
     }
 
     async componentDidMount() {
         this.props.onLoading();
 
-        await this._setupRipe();
+        await this.setupRipe();
 
-        this.state.ripeData.bind("pre_config", () => {
-            this.setState(
-                {
-                    loading: true,
-                    selectedPart: null,
-                    selectedMaterial: null,
-                    selectedColor: null
-                },
-                () => {
-                    this.props.onLoading();
-                }
-            );
+        console.log("pickers", this.props.ripe.brand);
+        console.log("pickers state", this.state.ripeData.brand);
+
+        this.onChoices = this.state.ripeData.bind("choices", choices => {
+            this.setState({ choices: choices });
         });
 
-        this.state.ripeData.bind("post_config", () => {
-            this.setState(
-                {
-                    loading: false,
-                    options: this._filteredOptions()
-                },
-                () => {
-                    this.props.onLoaded();
-                }
-            );
+        this.onPreConfig = this.state.ripeData.bind("pre_config", () => {
+            // reset the current selection state
+            // every time the config changes
+            this.setState({
+                selectedPart: null,
+                selectedMaterial: null,
+                selectedColor: null
+            });
         });
+
+        this.setState({ choices: this.state.ripeData.choices }, () => { this.props.onLoaded(); console.log(this.state.ripeData.choices) });
     }
 
-    /**
-     * Initializes RIPE instance if it does not exists and
-     * configures it with the given brand, model, version
-     * and parts. If a RIPE instance is provided, it will
-     * be used without further configuration.
-     */
-    async _setupRipe() {
-        if (!this.state.ripeData) {
-            this.setState(
-                { ripeData: new Ripe() },
-                async () => await this.state.ripeData.isReady()
-            );
-        } else {
-            await this.state.ripeData.isReady();
-        }
-
-        this.setState({ loading: false, options: this._filteredOptions() }, () =>
-            this.props.onLoaded()
-        );
-
-        if (global.ripe) return;
-        global.ripe = this.state.ripeData;
+    async componentWillUnmount() {
+        if (this.onPreConfig) this.state.ripeData.unbind("pre_config", this.onPreConfigEvent);
+        if (this.onChoices) this.state.ripeData.unbind("choices", this.onChoices);
     }
 
     _filteredOptions() {
         const choices = {};
 
-        for (const [part, partValue] of Object.entries(this.state.ripeData.choices)) {
+        for (const [part, partValue] of Object.entries(this.state.choices || {})) {
             if (!partValue.available) continue;
 
             const materials = {};
@@ -162,6 +167,31 @@ export class RipePickers extends Component {
         return choices;
     }
 
+    _partsOptions() {
+        return Object.keys(this._filteredOptions()).map(part => ({
+            label: this._normalize(part),
+            value: part
+        }));
+    }
+
+    _materialOptions() {
+        if (!this.state.selectedPart) return [];
+
+        return Object.keys(this._filteredOptions()[this.state.selectedPart]).map(material => ({
+            label: this._normalize(material),
+            value: material
+        }));
+    }
+
+    _colorOptions() {
+        if (!this.state.selectedMaterial) return [];
+
+        return this._filteredOptions()[this.state.selectedPart][this.state.selectedMaterial].map(color => ({
+            label: this._normalize(color),
+            value: color
+        }));
+    }
+
     _normalize(value) {
         return value
             .split("_")
@@ -176,13 +206,21 @@ export class RipePickers extends Component {
     }
 
     _onSelectPartChange(value) {
-        this.setState({ selectedPart: value, selectedMaterial: null, selectedColor: null }, () =>
+        if (!value) return;
+
+        const materialOptions = this._materialOptions();
+        const selectedMaterial = materialOptions.length === 1 ? materialOptions[0].value : null;
+        this.setState({ selectedPart: value, selectedMaterial: selectedMaterial, selectedColor: null }, () =>
             this.props.onUpdateHighlightedPart(value)
         );
     }
 
     _onSelectMaterialChange(value) {
-        this.setState({ selectedMaterial: value });
+        if (!value) return;
+
+        const colorOptions = this._colorOptions();
+        const selectedColor = colorOptions.length === 1 ? colorOptions[0].value : null;
+        this.setState({ selectedMaterial: value, selectedColor: selectedColor });
     }
 
     _onSelectColorChange(value) {
@@ -198,7 +236,6 @@ export class RipePickers extends Component {
             this.state.selectedColor
         );
 
-        this.props.onUpdateParts(this.state.ripeData.options.parts);
         this.setState({
             selectedPart: null,
             selectedMaterial: null,
@@ -209,19 +246,20 @@ export class RipePickers extends Component {
     render() {
         return (
             <div className="ripe-pickers">
-                <select
-                    value={this.state.selectedPart ? this.state.selectedPart : ""}
-                    onChange={event => this._onSelectPartChange(event.target.value)}
-                >
-                    <option value="" disabled>
-                        Parts
-                    </option>
-                    {Object.keys(this.state.options).map((part, index) => (
-                        <option value={part} key={`part-${index}`}>
-                            {this._normalize(part)}
+                {this._partsOptions().length > 0 &&
+                    <select
+                        value={this.state.selectedPart ? this.state.selectedPart : ""}
+                        onChange={event => this._onSelectPartChange(event.target.value)}
+                    >
+                        <option value="" disabled>
+                            Parts
                         </option>
-                    ))}
-                </select>
+                        {this._partsOptions().map((part, index) => (
+                            <option value={part.value} key={`part-${index}`}>
+                                {part.label}
+                            </option>
+                        ))}
+                    </select>}
                 <select
                     value={this.state.selectedMaterial ? this.state.selectedMaterial : ""}
                     onChange={event => this._onSelectMaterialChange(event.target.value)}
@@ -230,9 +268,9 @@ export class RipePickers extends Component {
                         Materials
                     </option>
                     {this.state.selectedPart &&
-                        Object.keys(this.state.options[this.state.selectedPart]).map(material => (
-                            <option value={material} key={material}>
-                                {this._normalize(material)}
+                        this._materialOptions().map((material, index) => (
+                            <option value={material.value} key={`material-${index}`}>
+                                {material.label}
                             </option>
                         ))}
                 </select>
@@ -244,11 +282,9 @@ export class RipePickers extends Component {
                         Colors
                     </option>
                     {this.state.selectedMaterial &&
-                        this.state.options[this.state.selectedPart][
-                            this.state.selectedMaterial
-                        ].map(color => (
-                            <option value={color} key={color}>
-                                {this._normalize(color)}
+                        this._colorOptions().map((color, index) => (
+                            <option value={color.value} key={`color-${index}`}>
+                                {color.label}
                             </option>
                         ))}
                 </select>
--- react/components/organisms/ripe-pickers/ripe-pickers.stories.js
+++ react/components/organisms/ripe-pickers/ripe-pickers.stories.js
@@ -6,11 +6,28 @@ import { Ripe } from "ripe-sdk";
 import { RipePickers } from "./ripe-pickers";
 
 const ripe = new Ripe("dummy", "cube", {
-    version: 52
+    version: 52,
+    parts: {
+        side: {
+            color: "black",
+            material: "crocodile_cbe",
+            face: "side"
+        },
+        shadow: {
+            color: "default",
+            hidden: true,
+            material: "default"
+        },
+        top0_bottom: {
+            color: "fuchsia",
+            face: "side",
+            material: "suede_cbe"
+        }
+    }
 });
 
 storiesOf("Organisms", module)
     .addDecorator(withKnobs)
     .add("RipePickers", () => {
-        return <RipePickers ripe={ripe} />;
+        return <RipePickers ripe={ripe} config={false} />;
     });
